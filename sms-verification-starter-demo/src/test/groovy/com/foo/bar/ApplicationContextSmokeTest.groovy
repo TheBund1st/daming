f6e55a5dc2653a0f7d77abe920e3cf9ae0a2147b
Yugang Zhou <ygzhou@thoughtworks.com>
2018-11-21 22:11:37 +0800

    Avoid transite spring-boot-starter-aop

---
+++ sms-verification-starter-demo/src/test/groovy/com/foo/bar/ApplicationContextSmokeTest.groovy
@@ -0,0 +1,14 @@
+package com.foo.bar
+
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+
+@SpringBootTest
+class ApplicationContextSmokeTest extends Specification {
+
+    def "it should load application context"() {
+        when: "it launch the application context"
+
+        then: "it should load successfully"
+    }
+}
