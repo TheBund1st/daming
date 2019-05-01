12e6afc0446ceb84b4e135a64ca8b045b2e9eb8a
Yugang Zhou <ygzhou@thoughtworks.com>
2019-05-01 09:42:40 +0800

    #18, fix @since of DeleteFromRedis

--- sms-verification-core/src/main/java/com/thebund1st/daming/redis/DeleteFromRedis.java
+++ sms-verification-core/src/main/java/com/thebund1st/daming/redis/DeleteFromRedis.java
@@ -4,7 +4,7 @@ package com.thebund1st.daming.redis;
  * RestTemplate has different .delete(K k) signature in 1.x and 2.x.
  * This interface is used to decouple the Redis Adapter with RestTemplate.delete(K k) method.
  *
- * @since 0.9.4
+ * @since 0.9.5
  */
 public interface DeleteFromRedis {
     void delete(String key);
