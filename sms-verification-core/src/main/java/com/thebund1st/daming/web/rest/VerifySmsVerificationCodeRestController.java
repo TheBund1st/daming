a45c5c68256e2b472cd1457389012a36e9f6ba13
Yugang Zhou <ygzhou@thoughtworks.com>
2019-08-18 15:36:01 +0800

    Implementing error response for 400 verify invalid code

--- sms-verification-core/src/main/java/com/thebund1st/daming/web/rest/VerifySmsVerificationCodeRestController.java
+++ sms-verification-core/src/main/java/com/thebund1st/daming/web/rest/VerifySmsVerificationCodeRestController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @ConditionalOnProperty(prefix = "daming.jwt", name = "enabled", havingValue = "true", matchIfMissing = true)
 @RequiredArgsConstructor
 @RestController
@@ -21,7 +23,7 @@ public class VerifySmsVerificationCodeRestController {
 
 
     @DeleteMapping("#{endpointProperties.verifySmsVerificationCodePath}")
-    public SmsVerifiedJwtResource handle(@RequestBody VerifySmsVerificationCodeCommand command) {
+    public SmsVerifiedJwtResource handle(@Valid @RequestBody VerifySmsVerificationCodeCommand command) {
         smsVerificationCommandHandler.handle(command);
         return new SmsVerifiedJwtResource(smsVerifiedJwtIssuer.issue(command.getMobile(), command.getScope()));
     }
