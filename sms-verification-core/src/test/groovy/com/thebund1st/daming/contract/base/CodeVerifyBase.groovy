ab2998fb486a4486e3c72b31aa4c6d2bb077d661
Yugang Zhou <ygzhou@thoughtworks.com>
2019-08-18 15:02:43 +0800

    Implementing contracts for verifying code

--- sms-verification-core/src/test/groovy/com/thebund1st/daming/contract/base/CodeVerifyBase.groovy
+++ sms-verification-core/src/test/groovy/com/thebund1st/daming/contract/base/CodeVerifyBase.groovy
@@ -3,10 +3,20 @@ package com.thebund1st.daming.contract.base
 
 import com.thebund1st.daming.web.AbstractWebMvcTest
 
+import static com.thebund1st.daming.commands.VerifySmsVerificationCodeCommandFixture.aVerifySmsVerificationCodeCommand
+
 class CodeVerifyBase extends AbstractWebMvcTest {
 
     def setup() {
-        
+        def command = aVerifySmsVerificationCodeCommand()
+                .withScope("DEMO")
+                .sendTo("13912222273")
+                .codeIs("123456")
+                .build()
+        def jwt = "This is a JWT token contains scope and mobile"
+
+        and:
+        smsVerifiedJwtIssuer.issue(command.getMobile(), command.scope) >> jwt
     }
 
 }
